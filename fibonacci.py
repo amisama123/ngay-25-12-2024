2c81b4cfe5caf8a44b0220f007b61c49f6e05d15
amisama123 <quyentak24406h@st.uel.edu.vn>
2024-12-31 12:06:35 +0700

    25/12/2024

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+def fib (n):
+    if n<=2:
+        return 1
+    return fib(n-1)+fib(n-2)
+def list_fib(n):
+    for i in range (1,n+1):
+        f=fib(i)
+        print(f, end=' ')
+    print()
+print(list_fib(6))
